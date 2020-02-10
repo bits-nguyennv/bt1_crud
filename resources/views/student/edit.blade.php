ddc8120125c31e16dca8bcf9835836d083ca575f
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2020-02-10 15:50:13 +0900

    change1

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -1,6 +1,5 @@
 @extends('layout')
 @section('content')  
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="{{ asset('css/bootstrap.min.css') }}">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
